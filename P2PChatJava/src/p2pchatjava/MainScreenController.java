b63311310d29249a0b76ad180508cdcf3067f4d7
CarlosOriol <carlos.oriol@upr.edu>
2020-05-01 03:00:11 -0400

    fixed layout and listview wrap text

--- P2PChatJava/src/p2pchatjava/MainScreenController.java
+++ P2PChatJava/src/p2pchatjava/MainScreenController.java
@@ -15,54 +15,82 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
+import javafx.scene.text.Text;
 
 /**
  *
  * @author Carlos
  */
-public class FXMLDocumentController implements Initializable {
-    
+public class MainScreenController implements Initializable {
+
     //Setting Observable list for list view
     ObservableList<Message> messages = FXCollections.observableArrayList();
-    
-    
+
     @FXML
     private Label label;
-    
+
     @FXML
     private void handleSendAction(ActionEvent event) {
         String validator = txtMessage.getText().trim();
-        if(validator.equals("")){
+        if (validator.equals("")) {
             //System.out.println("Mensaje vacio");
             return;
         }
         Message message = new Message();
         message.setMessageText(txtMessage.getText());
         //String message = txtMessage.getText();
-        
+
         System.out.println(message);
         messages.add(message);
         txtMessage.clear();
-        
+
     }
-    
+
     @FXML
     private Button btnSend;
-    
+
     @FXML
     private ListView listMessage;
-    
+
     @FXML
     private TextArea txtMessage;
-    
-            
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         txtMessage.setWrapText(true);
         listMessage.setItems(messages);
-        
-    }    
-    
+        setWraptextListView();
+
+    }
+
+    public void setWraptextListView() {
+        listMessage.setCellFactory(param -> new ListCell<Message>() {
+            @Override
+            protected void updateItem(Message item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || item == null) {
+                    setGraphic(null);
+                    setText(null);
+                    // other stuff to do...
+
+                } else {
+
+                     //set the width's
+                    setMinWidth(10);
+                    setMaxWidth(10);
+                    setPrefWidth(10);
+
+                    // allow wrapping
+                    setWrapText(true);
+
+                    setText(item.toString());
+
+                }
+            }
+        });
+    }
+
 }
