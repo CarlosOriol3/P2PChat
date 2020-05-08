cc684a352b607d2e895793185368f30b3b9a10e9
Arnaldo <arnaldomtorres15@gmail.com>
2020-05-08 00:15:47 -0400

    I fucked up!

---
+++ JavaClient/src/main/Main.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.net.URL;
+import java.io.File;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getPackage().getClass().getResource("ui/ConnectScreen.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+
+        //set unresizable
+        stage.setResizable(false);
+
+        //set window title
+        stage.setTitle("P2PChat");
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+        System.exit(0);
+    }
+    
+}
